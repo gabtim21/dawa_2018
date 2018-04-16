241c042d6482d44961bba80ae4024f06e5d696a0
gabtim21 <gabtim.leon@tecsup.edu.pe>
2018-04-16 18:44:05 -0500

    Lab06

---
+++ Lab06/scripts/server.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+app = express(),
+user = require('./models/user'),
+http = require('http').Server(app),
+port = process.env.PORT || 3000,
+io = require('socket.io')(http);
+//SOCKET conexion permanente
+
+app.set('view engine', 'jade');
+
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res){
+	res.render('main');
+});
+
+io.on('connection', function(socket){
+	console.log('Usuario Conectado');
+	user.show(function(data){
+		socket.emit('listar',data);
+	});
+	socket.on('crear', function(data){
+		user.create(data,function(rpta){
+			io.emit('nuevo',rpta)
+		});
+		console.log(data);
+	});
+	socket.on('actualizar',function(data){
+		user.update(data,function(rpta){
+			io.emit('nuevo',rpta);
+		})
+	})
+	socket.on('disconnect', function(){
+		console.log('Usuario Desconectado');
+	});
+});
+
+http.listen(port, function(){
+	console.log('Servidor conectado en *:' + port);
+});
